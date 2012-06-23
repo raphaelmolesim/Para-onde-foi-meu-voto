b634ad42148fb420bf91e058db96e3fecfce85a8
Raphael Molesim <raphael.sm86@gmail.com>
2012-06-23 19:06:02 -0300

    Changed message for IE

--- app/assets/javascripts/warning.js
+++ app/assets/javascripts/warning.js
@@ -1,7 +1,7 @@
 var msg1 = "Did you know that your Internet Explorer is out of date?";
 var msg2 = "To get the best possible experience using our website we recommend that you upgrade to a newer version or other web browser. A list of the most popular web browsers can be found below.";
 var msg3 = "Just click on the icons to get to the download page";
-var br1 = "Internet Explorer 7+";
+var br1 = "Internet Explorer 9+";
 var br2 = "Firefox 3+";
 var br3 = "Safari 3+";
 var br4 = "Opera 9.5+";
